23dec3e5940d6ff86a76418f79cdc5af1c2c9647
austinjs <ajsprunger@gmail.com>
2022-03-28 16:27:59 -0600

    delete string from seed.js/update package-lock

--- functions/seed.js
+++ functions/seed.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 
 const db = mongoose.connection
 
-mongoose.connect('mongodb+srv://austinjs:wJPtkg7Ro1wtHaN5@cluster0.wnaue.mongodb.net/mountain-jam?retryWrites=true&w=majority')
+mongoose.connect('')
 
 db.on('connected', function () {
   console.log(`Connected to MongoDB ${db.name} for seeding at ${db.host}:${db.port}`)
