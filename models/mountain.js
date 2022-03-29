443d571df94bc8e080b1572f305488c691267509
austinjs <ajsprunger@gmail.com>
2022-03-29 09:27:20 -0600

    add commentSchema

--- models/mountain.js
+++ models/mountain.js
@@ -2,6 +2,21 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  visitDate: {
+    type: Date,
+    default: function() {
+      return new Date().toLocaleString().split(',')[0]
+    }
+  },
+  comment: String,
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+  photo: {type: String},
+}, {
+  timestamps: true
+})
+
+
 const mountainSchema = new Schema({
   name: String,
   latitude: Number,
