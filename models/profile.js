047c4f92f1f44743e39b1a29847e1445537a49fb
Mikhail <mishanyaveselyi@gmail.com>
2022-03-30 11:38:33 -0400

    Edit profile model

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  visited:[{type: mongoose.Schema.Types.ObjectId, ref: "Mountain"}],
+  toVisit:[{type: mongoose.Schema.Types.ObjectId, ref: "Mountain"}]
 },{
     timestamps: true,
 })
