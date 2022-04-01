ae506b631f9d7614d7bdc1fb24427b567277fb09
austinjs <ajsprunger@gmail.com>
2022-03-31 18:21:36 -0600

    cleaned up obsolete code

--- routes/mountains.js
+++ routes/mountains.js
@@ -6,7 +6,6 @@ const router = Router()
 // Public Routes
 router.get('/', mountainsCtrl.index)
 router.get('/:id', mountainsCtrl.show)
-router.get('/search/:search', mountainsCtrl.search)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
